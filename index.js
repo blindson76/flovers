fd35fb5b11eb51b9b2e1431a5931d07a2d77e912
umit <blindson76@gmali.com>
2019-04-30 00:22:41 +0300

    d

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
-const tf = require('@tensorflow/tfjs')
+const tf = require('@tensorflow/tfjs'),
+Promise = require("bluebird");
 //require('@tensorflow/tfjs-node')
-,Jimp = require('jimp');
+Jimp = require('jimp');
 
 const express=require("express"),
 formidable = require('formidable')
@@ -39,26 +40,30 @@ app.post("/form",(req,res)=>{
 	})
 });
 app.post("/classify",(req,res)=>{
-	let results=[];
+	let files=[];
 	new formidable.IncomingForm().parse(req)
 	.on("file",(name,file)=>{
-		Jimp.read(file.path, (err, img) => {
-			img.resize(96, 96)   
-
-			let sen=tf.tensor(img.bitmap.data, [1,96,96,4])
-				.gather([3,2,1,0],3)
-				.slice([0,0,0,1],[1,96,96,3])
-				.div(tf.scalar(255))
-
-			results.push(model.predict(sen).dataSync());
-		});
+		files.push(Jimp.read(file.path))		
 
 	})
 	.on("fileBegin",(name,file)=>{
 		file.path = __dirname + '/uploads/' + file.name
 	})
-	.on("end",()=>{		
-		   res.end(JSON.stringify(results))
+	.on("end",()=>{	
+		let results=[];
+		Promise.all(files)
+		.then(images=>{
+			images.forEach(img=>{
+				img.resize(96, 96)   
+				let sen=tf.tensor(img.bitmap.data, [1,96,96,4])
+					.gather([3,2,1,0],3)
+					.slice([0,0,0,1],[1,96,96,3])
+					.div(tf.scalar(255))
+			   results.push(model.predict(sen).dataSync())
+
+			})
+			res.send(JSON.stringify(results))
+		})
 	})
 });
 app.get("/",(req,res)=>{
