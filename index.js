9ea5722a8a3fa0c7ad09b2d28aa90f7fb57101f2
umit <blindson76@gmali.com>
2019-05-03 12:04:25 +0300

    s

--- index.js
+++ index.js
@@ -42,30 +42,36 @@ app.post("/form",(req,res)=>{
 });
 app.post("/classify",(req,res)=>{
 	let files=[];
-	new formidable.IncomingForm().parse(req)
-	.on("file",(name,file)=>{
-		files.push(Jimp.read(file.path))		
+	try{
+		new formidable.IncomingForm().parse(req)
+		.on("file",(name,file)=>{
+			files.push(Jimp.read(file.path))		
 
-	})
-	.on("fileBegin",(name,file)=>{
-		file.path = __dirname + '/uploads/' + file.name
-	})
-	.on("end",()=>{	
-		let results=[];
-		Promise.all(files)
-		.then(images=>{
-			images.forEach(img=>{
-				img.resize(96, 96)   
-				let sen=tf.tensor(img.bitmap.data, [1,96,96,4])
-					.gather([3,2,1,0],3)
-					.slice([0,0,0,1],[1,96,96,3])
-					.div(tf.scalar(255))
-			   results.push(model.predict(sen).dataSync())
-
-			})
-			res.send(JSON.stringify(results))
 		})
-	})
+		.on("fileBegin",(name,file)=>{
+			file.path = __dirname + '/uploads/' + file.name
+		})
+		.on("end",()=>{	
+			let results=[];
+			Promise.all(files)
+			.then(images=>{
+				images.forEach(img=>{
+					img.resize(96, 96)   
+					let sen=tf.tensor(img.bitmap.data, [1,96,96,4])
+						.gather([3,2,1,0],3)
+						.slice([0,0,0,1],[1,96,96,3])
+						.div(tf.scalar(255))
+				   results.push(model.predict(sen).dataSync())
+
+				})
+				res.send(JSON.stringify(results))
+			})
+		})
+
+	}
+	catch(e){
+		res.send("ERROR")
+	}
 });
 app.get("/",(req,res)=>{
 	res.send("OK")
