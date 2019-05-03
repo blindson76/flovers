30684eed8620a30b900bb2dfadffa6db3f3252d3
umit <blindson76@gmali.com>
2019-05-03 12:38:07 +0300

    s

--- index.js
+++ index.js
@@ -67,6 +67,9 @@ app.post("/classify",(req,res)=>{
 				res.send(JSON.stringify(results))
 			})
 		})
+		.on("error",err=>{
+			res.send(JSON.stringify(err))
+		})
 
 	}
 	catch(e){
@@ -77,7 +80,7 @@ app.get("/",(req,res)=>{
 	res.send("OK")
 })
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}!`)
+	console.log('Example app listening on: '+url)
 
 	tf.loadLayersModel(url+'model.json')
 	.then(mdl=>{
@@ -90,74 +93,3 @@ app.listen(port, () => {
 	})
     
 })
-
-/**
-var Jimp = require('jimp');
-
-
-const labels=["Daisy","Dandelion","Rose","Sunflower","Tulip"];
-
-tf.loadLayersModel('file://model/model.json')
-.then(model=>{
-
-	Jimp.read('lena-small-bw.jpg', (err, lenna) => {
-	  if (err) throw err;
-	  lenna
-	    .resize(96, 96) // resize
-	    .quality(60) // set JPEG quality
-	    .greyscale() // set greyscale
-
-	   lenna.getBuffer(Jimp.MIME_BMP,(er,res)=>{
-	   	//console.log(res.length)
-	   })
-
-	   var sen=tf.tensor(lenna.bitmap.data, [1,96,96,4])
-	   let s=tf.slice(sen,[0,0,0,1],[1,96,96,3]);
-	   let pre=s.div(tf.scalar(255))
-	   console.log(labels[model.predict(pre).argMax(1).dataSync()[0]])
-	});
-})
-.catch(err=>{
-	console.error(err)
-})
-
-
-
-
-// open a file called "lenna.png"
-const express = require('express'),
-	 app = express(),
-	 port = 3000,
-	 formidable = require('formidable'),
-	 python=require("python-shell"),
-	 child=require('child_process'),
-	 path=require("path");
-
-let pyshell = new python.PythonShell('Main_Module.py',{
-	pythonPath:'C:/Users/user/Desktop/Kod/venv/Scripts/python.exe',
-	scriptPath:'C:/Users/user/Desktop/Kod/',
-	pythonOptions: ['-u'],
-	cwd:'C:/Users/user/Desktop/Kod/'
-});
-pyshell.on("message",(msg)=>{
-	pyshell.removeAllListeners("message")
-	
-})
-app.get('/', (req, res) => res.send('Hello World!'))
-app.post("/form",(req,res)=>{
-	new formidable.IncomingForm().parse(req)
-	.on("file",(name,file)=>{
-		pyshell.send(file.path)
-		pyshell.on("message",(msg)=>{
-			console.log(msg)
-			pyshell.removeAllListeners("message")
-			res.end(msg)
-		})
-	})
-	.on("fileBegin",(name,file)=>{
-		file.path = __dirname + '/uploads/' + file.name
-	})
-})
-app.use(express.static('static'))
-
-**/
